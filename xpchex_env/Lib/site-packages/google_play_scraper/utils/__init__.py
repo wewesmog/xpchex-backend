d6775ff2fcd67e6ae414e3b573379cccc72df063
Wes <wesleymogaka@gmail.com>
2026-03-01 00:07:09 +0300

    added more pages

--- xpchex_env/Lib/site-packages/google_play_scraper/utils/__init__.py
+++
@@ -1,8 +0,0 @@
-def nested_lookup(source, indexes):
-    try:
-        if len(indexes) == 1:
-            return source[indexes[0]]
-
-        return nested_lookup(source[indexes[0]], indexes[1:])
-    except TypeError:
-        return None
